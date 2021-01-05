c9cbf3c1d8ef3b50ea661632110ebc0b08ea23a0
Tamara Chuma <55622178+tamarachuma@users.noreply.github.com>
2021-01-05 19:19:20 +0400

    uploading website

---
+++ contactform.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "info@futurewinery.ge";
+    $headers = "From: ".$mailForm;
+    $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.html?mailsend");
+}
+
+?>
